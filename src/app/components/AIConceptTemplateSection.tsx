f7d9fb3cbec1e7cf816dc445e888f4474fb5dabe
jmathtech <jamil.matheny@gmail.com>
2025-08-09 20:43:39 -0400

    Adding new logo

--- src/app/components/AIConceptTemplateSection.tsx
+++ src/app/components/AIConceptTemplateSection.tsx
@@ -227,15 +227,7 @@ const AIConceptTemplateSection: React.FC<AIConceptTemplateSectionProps> = ({
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-2"><path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" /></svg>
                 Download HTML
-              </button>
-              <a href="#pricing">
-                <button
-                  className="flex items-center justify-center gap-8 w-80 px-6 py-3 mt-4 font-semibold text-white transition-all duration-300 cursor-pointer rounded-md shadow-md bg-green-600 hover:bg-green-500"
-                  type="button"
-                >
-                  Like it? See Pricing & Build It Live
-                </button>
-              </a>
+              </button>              
             </div>
           </div>
         )}
