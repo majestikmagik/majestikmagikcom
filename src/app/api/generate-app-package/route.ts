30e0d15e56cbebdb26f752f746414ad0048e36dc
jmathtech <jamil.matheny@gmail.com>
2025-11-11 21:36:46 -0500

    Adding runtime for the generate application - nodejs.

--- src/app/api/generate-app-package/route.ts
+++ src/app/api/generate-app-package/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import JSZip from 'jszip';
 
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 export async function POST(req: NextRequest) {
   try {
